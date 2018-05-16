204039ec5a115a2eee2997318c4c13996ba2960c
Jacob Sansbury <jacobsansbu@icloud.com>
2018-05-16 04:46:49 -0400

    Config anyone?

---
+++ src/config/deck.config.js
@@ -0,0 +1,21 @@
+const config = {
+    types: {
+        jack: 10,
+        queen: 10,
+        king: 10,
+        ten: 10,
+        nine: 9,
+        eight: 8,
+        seven: 7,
+        six: 6,
+        five: 5,
+        four: 4,
+        three: 3,
+        two: 2,
+        one: 1,
+        ace: 1
+    },
+    suits:  ['clubs', 'diamonds', 'hearts', 'spades']
+};
+
+export default config;
